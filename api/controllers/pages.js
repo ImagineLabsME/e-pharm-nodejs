297d631d6d28e5ea968ad05d63ed9b59af8e3f38
sirajmhanna <59828433+sirajmhanna@users.noreply.github.com>
2021-02-12 00:42:33 +0200

    Update pages.js

--- api/controllers/pages.js
+++ api/controllers/pages.js
@@ -19,7 +19,7 @@ exports.page = (req, res) => {
                 break;
             case 'viewListings': data = req.query.lang === 'EN' ? pages.viewListings.EN : pages.viewListings.AR;
                 break;
-            case 'addListing': data = req.query.lang === 'EN' ? pages.addListings.EN : pages.addListings.AR;
+            case 'addListing': data = req.query.lang === 'EN' ? pages.addListing.EN : pages.addListing.AR;
                 break;
                 case 'notFound': data = req.query.lang === 'EN' ? pages.notFound.EN : pages.notFound.AR;
                 break;
